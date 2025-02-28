f9ba867a09a1fc495fb2985b8c3e95af948c451b
Medet Ahmetson <milayter@gmail.com>
2025-02-28 11:18:59 +0700

    Replace the main page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,16 @@ import { defineConfig } from "astro/config";
 import { CONFIG } from "./src/config";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
+import tailwindcss from "@tailwindcss/vite";
 
 import playformCompress from "@playform/compress";
+
+import react from "@astrojs/react";
 
 export default defineConfig({
   base: "/",
   site: CONFIG.site_url,
-  integrations: [sitemap(), mdx(), playformCompress()],
+  integrations: [sitemap(), mdx(), playformCompress(), react()],
   markdown: {
     shikiConfig: {
       theme: "material-theme-darker",
@@ -24,5 +27,8 @@ export default defineConfig({
         schema: "src/content/config.ts#finds",
       },
     },
+  },
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
