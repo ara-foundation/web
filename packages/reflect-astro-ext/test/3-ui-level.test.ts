8edd61a424d8d40b4637905b85bb4bbe55724a98
Medet Ahmetson <medet@ara.foundation>
2025-05-02 19:42:03 +0700

    @ara-web/reflect-astro-ext is updating the tests to work with page level for any modules

--- packages/reflect-astro-ext/test/3-ui-level.test.ts
+++ packages/reflect-astro-ext/test/3-ui-level.test.ts
@@ -5,7 +5,7 @@
 
 import { expect, test } from "vitest";
 import { ModuleMemory } from "@ara-web/reflect";
-import { ModuleCategory, ModulePartitioner, CodeLevel, PageLevel, type Page, FileExtension, Component } from "../src";
+import { ModuleCategory, ModulePartitioner, CodeLevel, PageLevel, type Page, FileExtension, Component, ModuleIdentifier, Asset, Script } from "../src";
 import { getImportRecords, getNewAstroReflect, getNewProjectMemory } from "./shared";
 import { Debug } from "@ara-web/ts-enhancement";
 
@@ -85,3 +85,122 @@ test(`Make sure the that pages JSON are automatically updated`, async () => {
         // }
     }
 })
+
+test(`Make sure the that components are generated`, async () => {
+    const modules = getImportRecords()
+          
+    const reflectExtension = await getNewAstroReflect();
+    const validated = await reflectExtension.putModules(modules);
+    expect(validated.isSuccess).toBe(true);
+    // Make sure they are all no content moduled
+    const projectMemory = getNewProjectMemory(reflectExtension);
+        
+    const moduleMemories = projectMemory.getModules();
+    expect(Object.keys(moduleMemories).length).toBeGreaterThan(0);
+    let componentFound = false;
+    for (let moduleMemory of moduleMemories) {
+        const moduleParts = await ModulePartitioner.partition(moduleMemory);
+        expect(moduleParts.isSuccess).toBe(true);
+        
+        if (moduleParts.getValue().fileExtension !== FileExtension.Astro) {
+            continue;
+        } 
+        if (moduleMemory.moduleCategory !== ModuleCategory.Component) {
+            continue;
+        }
+        componentFound = true;
+        const identifiedSourceCode = await CodeLevel.identifySourceCode<Page>(moduleParts.getValue().source, moduleMemory as ModuleMemory<Page>, projectMemory);
+        expect(identifiedSourceCode.isSuccess).toBe(true);
+        const identifiedModule = await PageLevel.identify<Page>(moduleParts.getValue(), identifiedSourceCode.getValue());
+        expect(identifiedModule.isSuccess).toBe(true);
+    }
+    expect(componentFound).toBe(true);
+});
+
+test(`Make sure the that layouts are generated`, async () => {
+    const modules = getImportRecords()
+          
+    const reflectExtension = await getNewAstroReflect();
+    const validated = await reflectExtension.putModules(modules);
+    expect(validated.isSuccess).toBe(true);
+    // Make sure they are all no content moduled
+    const projectMemory = getNewProjectMemory(reflectExtension);
+        
+    const moduleMemories = projectMemory.getModules();
+    expect(Object.keys(moduleMemories).length).toBeGreaterThan(0);
+    let layoutFound = false;
+    for (let moduleMemory of moduleMemories) {
+        const moduleParts = await ModulePartitioner.partition(moduleMemory);
+        expect(moduleParts.isSuccess).toBe(true);
+        
+        if (moduleParts.getValue().fileExtension !== FileExtension.Astro) {
+            continue;
+        } 
+        if (moduleMemory.moduleCategory !== ModuleCategory.Layout) {
+            continue;
+        }
+        layoutFound = true;
+        const identifiedSourceCode = await CodeLevel.identifySourceCode<Page>(moduleParts.getValue().source, moduleMemory as ModuleMemory<Page>, projectMemory);
+        expect(identifiedSourceCode.isSuccess).toBe(true);
+        const identifiedModule = await PageLevel.identify<Page>(moduleParts.getValue(), identifiedSourceCode.getValue());
+        expect(identifiedModule.isSuccess).toBe(true);
+    }
+    expect(layoutFound).toBe(true);
+});
+
+test(`Make sure the that scripts are generated`, async () => {
+    const modules = getImportRecords()
+          
+    const reflectExtension = await getNewAstroReflect();
+    const validated = await reflectExtension.putModules(modules);
+    expect(validated.isSuccess).toBe(true);
+    // Make sure they are all no content moduled
+    const projectMemory = getNewProjectMemory(reflectExtension);
+        
+    const moduleMemories = projectMemory.getModules();
+    expect(Object.keys(moduleMemories).length).toBeGreaterThan(0);
+    let scriptFound = false;
+    for (let moduleMemory of moduleMemories) {
+        const moduleParts = await ModulePartitioner.partition(moduleMemory);
+        expect(moduleParts.isSuccess).toBe(true);
+        
+        const extension = moduleParts.getValue().fileExtension
+        if (!ModuleIdentifier.isScript(extension)) {
+            continue;
+        }
+
+        scriptFound = true;
+        const identifiedModule = await ModuleIdentifier.identify<Script>(moduleParts.getValue(), moduleMemory as ModuleMemory<Script>);
+        expect(identifiedModule.isSuccess).toBe(true);
+    }
+    expect(scriptFound).toBe(true);
+});
+
+test(`Make sure that the assets are generated`, async () => {
+    const modules = getImportRecords()
+          
+    const reflectExtension = await getNewAstroReflect();
+    const validated = await reflectExtension.putModules(modules);
+    expect(validated.isSuccess).toBe(true);
+    // Make sure they are all no content moduled
+    const projectMemory = getNewProjectMemory(reflectExtension);
+        
+    const moduleMemories = projectMemory.getModules();
+    expect(Object.keys(moduleMemories).length).toBeGreaterThan(0);
+    let assetFound = false;
+    for (let moduleMemory of moduleMemories) {
+        const moduleParts = await ModulePartitioner.partition(moduleMemory);
+        expect(moduleParts.isSuccess).toBe(true);
+        
+        const extension = moduleParts.getValue().fileExtension
+        if (!ModuleIdentifier.isAsset(extension)) {
+            continue;
+        }
+
+        assetFound = true;
+        const identifiedModule = await ModuleIdentifier.identify<Asset>(moduleParts.getValue(), moduleMemory as ModuleMemory<Asset>);
+        expect(identifiedModule.isSuccess).toBe(true);
+    }
+    expect(assetFound).toBe(true);
+});
+
